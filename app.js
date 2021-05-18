0aed74fb8f2a8d3293a4c02d37f2d49dc1373198
Pavlin <forex.xard@gmail.com>
2021-05-18 11:24:52 -0400

    created serrver and app base

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const ExpressError = require('./helpers/expressError');
+
+
+app.use(express.json());
+
+app.get('/', (req, res) => res.send('Hello World'))
+
+app.use((req, res, next) => {
+    const err = new ExpressError("Not Found", 404)
+
+    return next(err);
+})
+
+app.use((err, req, res, next) => {
+    res.status((err.status || 500));
+
+    return res.json({
+        status: err.status,
+        message: err.message
+    })
+})
+
+module.exports = app;
