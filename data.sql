589e81ada4e070412f0aff6479d68cc3760cf7a6
Pavlin <forex.xard@gmail.com>
2021-05-21 11:21:16 -0400

    added new table to data base for order details

--- data.sql
+++ data.sql
@@ -41,6 +41,8 @@ CREATE TABLE orders (
     order_date DATE DEFAULT CURRENT_DATE
 );
 
+
+
 CREATE TABLE item(
     id SERIAL PRIMARY KEY,
     user_id INTEGER REFERENCES users ON DELETE CASCADE,
@@ -50,4 +52,13 @@ CREATE TABLE item(
     price FLOAT,
     details TEXT,
     date DATE DEFAULT CURRENT_DATE
+);
+
+CREATE TABLE order_details (
+    id SERIAL PRIMARY KEY,
+    order_Id INTEGER REFERENCES orders ON DELETE CASCADE,
+    item_id INTEGER REFERENCES item ON DELETE CASCADE,
+    quantity FLOAT,
+    waigth FLOAT,
+    isqty BOOLEAN
 );
